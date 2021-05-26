b40371eac816f6192b9bf855b7e5743e55fd7736
ritikchn <rintendoplex@gmail.com>
2021-05-26 22:03:47 +0530

    new client id

--- src/Header.jsx
+++ src/Header.jsx
@@ -29,7 +29,7 @@ function Header(props) {
   const onFailure = (res) => {
     console.log("[Login failed] res:", res);
   };
-  const key ="950808696686-2a0tmdu6sc823b4f3uso2sp74meo4tta.apps.googleusercontent.com";
+  const key =process.env.REACT_APP_API_KEY;
 
   return (
     <div className="Header-parent">
