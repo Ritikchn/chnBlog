818686b4a25812214c3f60bc2386c55dff43a56e
ritikchn <rintendoplex@gmail.com>
2021-05-26 21:46:55 +0530

    direct key

--- src/Header.jsx
+++ src/Header.jsx
@@ -29,7 +29,7 @@ function Header(props) {
   const onFailure = (res) => {
     console.log("[Login failed] res:", res);
   };
-  const key =process.env.REACT_APP_API_KEY;
+  const key ="950808696686-o94jjqv72d34is4epvoddviac44in3p9.apps.googleusercontent.com";
 
   return (
     <div className="Header-parent">
